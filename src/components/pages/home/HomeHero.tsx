c46382cb23ca3358830a6fcc64905d5f3c5e5df5
victoryoung <vicmachine08@gmail.com>
2024-12-04 14:34:20 +1100

    hero adjustments

--- src/components/pages/home/HomeHero.tsx
+++ src/components/pages/home/HomeHero.tsx
@@ -23,29 +23,36 @@ export default function HomeHero() {
           </div>
           <div className="flex flex-col items-center justify-center gap-4">
             <div className="flex items-center relative">
-              <h1 className="w-fit">Your All-In-One Revenue Chasers</h1>
-              <div className="absolute right-[-16px] lg:bottom-[30%] lg:right-[60px] -rotate-6">
-                <CldImage
-                  name="dollar-icon_n8be85"
-                  alt="hand-right decoration"
-                  size={28}
-                  imageClass=""
-                />
-              </div>
-              <div className="absolute top-0 left-[-4px] lg:top-[-12px] lg:left-[80px] -rotate-6">
-                <CldImage
-                  name="cog-icon_xkxrnk"
-                  alt="cog decoration"
-                  size={28}
-                  imageClass=""
-                />
+              <div className="">
+                <span className="relative">
+                  <h1>Your All-In-One</h1>
+                  <div className="absolute -rotate-6 top-[-8px]">
+                    <CldImage
+                      name="cog-icon_xkxrnk"
+                      alt="cog decoration"
+                      size={28}
+                      imageClass=""
+                    />
+                  </div>
+                </span>
+                <span className="relative">
+                  <h1>Revenue Chasers</h1>
+                  <div className="absolute right-[-30px] bottom-[36px] -rotate-6">
+                    <CldImage
+                      name="dollar-icon_n8be85"
+                      alt="hand-right decoration"
+                      size={28}
+                      imageClass=""
+                    />
+                  </div>
+                </span>
               </div>
             </div>
 
             <p>
-              Ship marketing campaigns that drive revenue.
+              Ship marketing campaigns to drive revenue.
               <br />
-              Leads, pipeline, prospects - you name it.
+              Leads, pipeline, prospects - you name it, we&apos;ll get it.
             </p>
             <div className="md:max-w-md mx-auto md:w-full py-4">
               <button
